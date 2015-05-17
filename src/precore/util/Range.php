cf0ea7fc08192c02b713eb113f036b14e703fa9b
Janos Szurovecz <szjani@szjani.hu>
2015-05-17 18:08:38 +0200

    Add range support

---
+++ src/precore/util/Range.php
@@ -0,0 +1,580 @@
+<?php
+/*
+ * Copyright (c) 2012-2015 Janos Szurovecz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace precore\util;
+
+use BadMethodCallException;
+use precore\lang\ClassCastException;
+use precore\lang\Comparable;
+use precore\lang\NullPointerException;
+use precore\lang\Object;
+use precore\lang\ObjectInterface;
+use Traversable;
+
+/**
+ * Class Range
+ *
+ * @package precore\util
+ * @author Janos Szurovecz <szjani@szjani.hu>
+ */
+final class Range extends Object
+{
+    private static $ALL;
+
+    /**
+     * @var Cut
+     */
+    private $lowerBound;
+
+    /**
+     * @var Cut
+     */
+    private $upperBound;
+
+    private function __construct(Cut $lowerBound, Cut $upperBound)
+    {
+        Preconditions::checkArgument(
+            $lowerBound->compareTo($upperBound) <= 0
+            && $lowerBound !== Cut::aboveAll()
+            && $upperBound !== Cut::belowAll(),
+            'Invalid range: %s', self::toStringBounds($lowerBound, $upperBound)
+        );
+        $this->lowerBound = $lowerBound;
+        $this->upperBound = $upperBound;
+    }
+
+    private static function toStringBounds(Cut $lower, Cut $upper)
+    {
+        return $lower->describeAsLowerBound() . '..' . $upper->describeAsUpperBound();
+    }
+
+    private static function comparatorOrNatural(Comparator $comparator = null)
+    {
+        return $comparator !== null ? $comparator : Ordering::natural();
+    }
+
+    public static function init()
+    {
+        self::$ALL = new Range(Cut::belowAll(), Cut::aboveAll());
+    }
+
+    /**
+     * @param $lower
+     * @param $upper
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function open($lower, $upper, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::aboveValue($lower, $comparator), Cut::belowValue($upper, $comparator));
+    }
+
+    /**
+     * @param $lower
+     * @param $upper
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function closed($lower, $upper, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::belowValue($lower, $comparator), Cut::aboveValue($upper, $comparator));
+    }
+
+    /**
+     * @param $lower
+     * @param $upper
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function openClosed($lower, $upper, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::aboveValue($lower, $comparator), Cut::aboveValue($upper, $comparator));
+    }
+
+    /**
+     * @param $lower
+     * @param $upper
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function closedOpen($lower, $upper, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::belowValue($lower, $comparator), Cut::belowValue($upper, $comparator));
+    }
+
+    /**
+     * @return Range
+     */
+    public static function all()
+    {
+        return self::$ALL;
+    }
+
+    /**
+     * @param $endpoint
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function greaterThan($endpoint, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::aboveValue($endpoint, $comparator), Cut::aboveAll());
+    }
+
+    /**
+     * @param $endpoint
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function lessThan($endpoint, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::belowAll(), Cut::belowValue($endpoint, $comparator));
+    }
+
+    /**
+     * @param $endpoint
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function atLeast($endpoint, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::belowValue($endpoint, $comparator), Cut::aboveAll());
+    }
+
+    /**
+     * @param $endpoint
+     * @param Comparator $comparator
+     * @return Range
+     */
+    public static function atMost($endpoint, Comparator $comparator = null)
+    {
+        $comparator = self::comparatorOrNatural($comparator);
+        return new Range(Cut::belowAll(), Cut::aboveValue($endpoint, $comparator));
+    }
+
+    public function __invoke($element)
+    {
+        return $this->contains($element);
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function contains($value)
+    {
+        Preconditions::checkNotNull($value);
+        return $this->lowerBound->isLessThan($value) && !$this->upperBound->isLessThan($value);
+    }
+
+    /**
+     * @param Traversable $elements
+     * @return bool
+     */
+    public function containsAll(Traversable $elements)
+    {
+        return Iterators::all(Iterators::from($elements), $this);
+    }
+
+    public function isEmpty()
+    {
+        return $this->lowerBound->equals($this->upperBound);
+    }
+
+    public function lowerEndpoint()
+    {
+        return $this->lowerBound->endpoint();
+    }
+
+    public function upperEndpoint()
+    {
+        return $this->upperBound->endpoint();
+    }
+
+    /**
+     * @param Range $other
+     * @return boolean
+     */
+    public function encloses(Range $other)
+    {
+        return $this->lowerBound->compareTo($other->lowerBound) <= 0
+            && $this->upperBound->compareTo($other->upperBound) >= 0;
+    }
+
+    public function isConnected(Range $other)
+    {
+        return $this->lowerBound->compareTo($other->upperBound) <= 0
+            && $other->lowerBound->compareTo($this->upperBound) <= 0;
+    }
+
+    /**
+     * @param Range $connectedRange
+     * @return Range
+     */
+    public function intersection(Range $connectedRange)
+    {
+        $lowerCmp = $this->lowerBound->compareTo($connectedRange->lowerBound);
+        $upperCmp = $this->upperBound->compareTo($connectedRange->upperBound);
+        if ($lowerCmp >= 0 && $upperCmp <= 0) {
+            return $this;
+        } elseif ($lowerCmp <= 0 && $upperCmp >= 0) {
+            return $connectedRange;
+        } else {
+            $newLower = ($lowerCmp >= 0) ? $this->lowerBound : $connectedRange->lowerBound;
+            $newUpper = ($upperCmp <= 0) ? $this->upperBound : $connectedRange->upperBound;
+            return new Range($newLower, $newUpper);
+        }
+    }
+
+    /**
+     * @param Range $other
+     * @return Range
+     */
+    public function span(Range $other)
+    {
+        $lowerCmp = $this->lowerBound->compareTo($other->lowerBound);
+        $upperCmp = $this->upperBound->compareTo($other->upperBound);
+        if ($lowerCmp <= 0 && $upperCmp >= 0) {
+            return $this;
+        } elseif ($lowerCmp >= 0 && $upperCmp <= 0) {
+            return $other;
+        } else {
+            $newLower = ($lowerCmp <= 0) ? $this->lowerBound : $other->lowerBound;
+            $newUpper = ($upperCmp >= 0) ? $this->upperBound : $other->upperBound;
+            return new Range($newLower, $newUpper);
+        }
+    }
+
+    public function equals(ObjectInterface $object = null)
+    {
+        return $object instanceof Range
+            && $this->lowerBound->equals($object->lowerBound)
+            && $this->upperBound->equals($object->upperBound);
+    }
+
+    public function toString()
+    {
+        return self::toStringBounds($this->lowerBound, $this->upperBound);
+    }
+}
+
+/**
+ * It is not intended to be used in your code.
+ *
+ * @package precore\util
+ * @author Janos Szurovecz <szjani@szjani.hu>
+ */
+abstract class Cut extends Object implements Comparable
+{
+    private $endpoint;
+
+    /**
+     * @var Comparator
+     */
+    private $comparator;
+
+    public function __construct($endpoint, Comparator $comparator = null)
+    {
+        $this->endpoint = $endpoint;
+        $this->comparator = $comparator;
+    }
+
+    /**
+     * @return string
+     */
+    public abstract function describeAsLowerBound();
+
+    /**
+     * @return string
+     */
+    public abstract function describeAsUpperBound();
+
+    /**
+     * @return Cut
+     */
+    public static function belowAll()
+    {
+        return BelowAll::instance();
+    }
+
+    /**
+     * @return Cut
+     */
+    public static function aboveAll()
+    {
+        return AboveAll::instance();
+    }
+
+    /**
+     * @param $value
+     * @param Comparator $comparator
+     * @return Cut
+     */
+    public static function belowValue($value, Comparator $comparator)
+    {
+        return new BelowValue($value, $comparator);
+    }
+
+    /**
+     * @param $value
+     * @param Comparator $comparator
+     * @return Cut
+     */
+    public static function aboveValue($value, Comparator $comparator)
+    {
+        return new AboveValue($value, $comparator);
+    }
+
+    public abstract function isLessThan($value);
+
+    public function endpoint()
+    {
+        return $this->endpoint;
+    }
+
+    /**
+     * @return Comparator
+     */
+    protected function comparator()
+    {
+        return $this->comparator;
+    }
+
+    /**
+     * @param $object
+     * @return int a negative integer, zero, or a positive integer
+     *         as this object is less than, equal to, or greater than the specified object.
+     * @throws ClassCastException - if the specified object's type prevents it from being compared to this object.
+     * @throws NullPointerException if the specified object is null
+     */
+    public function compareTo($object)
+    {
+        Cut::objectClass()->cast(Preconditions::checkNotNull($object));
+        /* @var $object Cut */
+        if ($object === self::belowAll()) {
+            return 1;
+        }
+        if ($object === self::aboveAll()) {
+            return -1;
+        }
+        $result = $this->comparator->compare($this->endpoint, $object->endpoint);
+        if ($result !== 0) {
+            return $result;
+        }
+        return Booleans::compare($this instanceof AboveValue, $object instanceof AboveValue);
+    }
+
+    public function equals(ObjectInterface $object = null)
+    {
+        return $object !== null
+            && $this->getClassName() === $object->getClassName()
+            && $this->compareTo($object) === 0;
+    }
+}
+
+/**
+ * It is not intended to be used in your code.
+ *
+ * @package precore\util
+ * @author Janos Szurovecz <szjani@szjani.hu>
+ */
+final class BelowAll extends Cut
+{
+    private static $INSTANCE;
+
+    public static function init()
+    {
+        self::$INSTANCE = new BelowAll(null, null);
+    }
+
+    public static function instance()
+    {
+        return self::$INSTANCE;
+    }
+
+    public function isLessThan($value)
+    {
+        return true;
+    }
+
+    public function compareTo($object)
+    {
+        return $object === $this ? 0 : -1;
+    }
+
+    public function toString()
+    {
+        return '-?';
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsLowerBound()
+    {
+        return '(' . $this->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsUpperBound()
+    {
+        throw new BadMethodCallException();
+    }
+}
+
+final class BelowValue extends Cut
+{
+    public function __construct($endpoint, Comparator $comparator = null)
+    {
+        parent::__construct(Preconditions::checkNotNull($endpoint), Preconditions::checkNotNull($comparator));
+    }
+
+    public function isLessThan($value)
+    {
+        return $this->comparator()->compare($this->endpoint(), $value) <= 0;
+    }
+
+    public function toString()
+    {
+        return $this->endpoint();
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsLowerBound()
+    {
+        return '[' . $this->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsUpperBound()
+    {
+        return $this->toString() . ')';
+    }
+}
+
+/**
+ * It is not intended to be used in your code.
+ *
+ * @package precore\util
+ * @author Janos Szurovecz <szjani@szjani.hu>
+ */
+final class AboveAll extends Cut
+{
+    private static $INSTANCE;
+
+    public static function init()
+    {
+        self::$INSTANCE = new AboveAll(null, null);
+    }
+
+    public static function instance()
+    {
+        return self::$INSTANCE;
+    }
+
+    public function isLessThan($value)
+    {
+        return false;
+    }
+
+    public function compareTo($object)
+    {
+        return $object === $this ? 0 : 1;
+    }
+
+    public function toString()
+    {
+        return '?';
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsLowerBound()
+    {
+        throw new BadMethodCallException();
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsUpperBound()
+    {
+        return $this->toString() . ')';
+    }
+}
+
+/**
+ * It is not intended to be used in your code.
+ *
+ * @package precore\util
+ * @author Janos Szurovecz <szjani@szjani.hu>
+ */
+final class AboveValue extends Cut
+{
+    public function __construct($endpoint, Comparator $comparator = null)
+    {
+        parent::__construct(Preconditions::checkNotNull($endpoint), Preconditions::checkNotNull($comparator));
+    }
+
+    public function isLessThan($value)
+    {
+        return $this->comparator()->compare($this->endpoint(), $value) < 0;
+    }
+
+    public function toString()
+    {
+        return $this->endpoint();
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsLowerBound()
+    {
+        return '(' . $this->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function describeAsUpperBound()
+    {
+        return $this->toString() . ']';
+    }
+}
+BelowAll::init();
+AboveAll::init();
+Range::init();
---
+++ tests/src/precore/util/RangeTest.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace precore\util;
+
+use PHPUnit_Framework_TestCase;
+use precore\util\concurrent\TimeUnit;
+
+/**
+ * Class RangeTest
+ *
+ * @package precore\util
+ * @author Janos Szurovecz <szjani@szjani.hu>
+ */
+class RangeTest extends PHPUnit_Framework_TestCase
+{
+    private $input = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+    /**
+     * @test
+     */
+    public function shouldCreateOpenRange()
+    {
+        $range = Range::open('b', 'e', Ordering::usingToString());
+        self::assertSame(['c', 'd'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateClosedRange()
+    {
+        $range = Range::closed('b', 'e', Ordering::usingToString());
+        self::assertSame(['b', 'c', 'd', 'e'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateOpenClosedRange()
+    {
+        $range = Range::openClosed('b', 'e', Ordering::usingToString());
+        self::assertSame(['c', 'd', 'e'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateClosedOpenRange()
+    {
+        $range = Range::closedOpen('b', 'e', Ordering::usingToString());
+        self::assertSame(['b', 'c', 'd'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateAllRange()
+    {
+        self::assertSame($this->input, FluentIterable::of($this->input)->filter(Range::all())->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateGreaterThanRange()
+    {
+        $range = Range::greaterThan('b', Ordering::usingToString());
+        self::assertSame(['c', 'd', 'e', 'f'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateAtLeastRange()
+    {
+        $range = Range::atLeast('b', Ordering::usingToString());
+        self::assertSame(['b', 'c', 'd', 'e', 'f'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateLessThanRange()
+    {
+        $range = Range::lessThan('e', Ordering::usingToString());
+        self::assertSame(['a', 'b', 'c', 'd'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateAtMostRange()
+    {
+        $range = Range::atMost('d', Ordering::usingToString());
+        self::assertSame(['a', 'b', 'c', 'd'], FluentIterable::of($this->input)->filter($range)->toArray());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldUseNaturalOrdering()
+    {
+        $range = Range::closed(TimeUnit::$SECONDS, TimeUnit::$HOURS);
+        $result = FluentIterable::of(TimeUnit::values())->filter($range);
+        self::assertTrue(Iterables::contains($result, TimeUnit::$SECONDS));
+        self::assertTrue(Iterables::contains($result, TimeUnit::$MINUTES));
+        self::assertTrue(Iterables::contains($result, TimeUnit::$HOURS));
+        self::assertFalse(Iterables::contains($result, TimeUnit::$MILLISECONDS));
+        self::assertFalse(Iterables::contains($result, TimeUnit::$DAYS));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCheckContains()
+    {
+        self::assertTrue(Range::closed('b', 'e', Ordering::usingToString())->contains('b'));
+        self::assertTrue(Range::closed('b', 'e', Ordering::usingToString())->contains('e'));
+        self::assertFalse(Range::closed('b', 'e', Ordering::usingToString())->contains('a'));
+        self::assertFalse(Range::open('b', 'e', Ordering::usingToString())->contains('b'));
+        self::assertTrue(Range::open('b', 'e', Ordering::usingToString())->contains('c'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCheckContainsAll()
+    {
+        $bc = Iterators::forArray(['b', 'c']);
+        self::assertTrue(Range::closed('b', 'e', Ordering::usingToString())->containsAll($bc));
+        self::assertFalse(Range::open('b', 'e', Ordering::usingToString())->containsAll($bc));
+        self::assertTrue(Range::closed('a', 'd', Ordering::usingToString())->containsAll($bc));
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function shouldThrowExceptionIfLowerIsHigherThanUpper()
+    {
+        Range::closed('b', 'a', Ordering::usingToString());
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function shouldThrowExceptionIfLowerIsEqualToUpper()
+    {
+        Range::open('a', 'a', Ordering::usingToString());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeEmpty()
+    {
+        self::assertTrue(Range::openClosed('a', 'a', Ordering::usingToString())->isEmpty());
+        self::assertTrue(Range::closedOpen('a', 'a', Ordering::usingToString())->isEmpty());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldEnclose()
+    {
+        $comparator = Numbers::naturalOrdering();
+        self::assertTrue(Range::closed(3, 6, $comparator)->encloses(Range::closed(4, 5, $comparator)));
+        self::assertTrue(Range::open(3, 6, $comparator)->encloses(Range::open(3, 6, $comparator)));
+        self::assertTrue(Range::closed(3, 6, $comparator)->encloses(Range::closedOpen(4, 4, $comparator)));
+        self::assertFalse(Range::openClosed(3, 6, $comparator)->encloses(Range::closed(3, 6, $comparator)));
+        self::assertFalse(Range::closed(4, 5, $comparator)->encloses(Range::open(3, 6, $comparator)));
+        self::assertFalse(Range::closed(3, 6, $comparator)->encloses(Range::openClosed(1, 1, $comparator)));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeConnected()
+    {
+        $comparator = Numbers::naturalOrdering();
+        self::assertTrue(Range::closed(3, 5, $comparator)->isConnected(Range::open(5, 10, $comparator)));
+        self::assertTrue(Range::closed(0, 9, $comparator)->isConnected(Range::closed(3, 4, $comparator)));
+        self::assertTrue(Range::closed(0, 5, $comparator)->isConnected(Range::closed(3, 9, $comparator)));
+        self::assertFalse(Range::open(3, 5, $comparator)->isConnected(Range::open(5, 10, $comparator)));
+        self::assertFalse(Range::closed(1, 5, $comparator)->isConnected(Range::closed(6, 10, $comparator)));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateIntersection()
+    {
+        $cmp = Numbers::naturalOrdering();
+        $res1 = Range::openClosed(5, 5, $cmp);
+        self::assertTrue(Range::closed(3, 5, $cmp)->intersection(Range::open(5, 10, $cmp))->equals($res1));
+
+        $res2 = Range::closed(3, 4, $cmp);
+        self::assertTrue(Range::closed(0, 9, $cmp)->intersection(Range::closed(3, 4, $cmp))->equals($res2));
+
+        $res3 = Range::closed(3, 5, $cmp);
+        self::assertTrue(Range::closed(0, 5, $cmp)->intersection(Range::closed(3, 9, $cmp))->equals($res3));
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function shouldThrowExceptionIfNoIntersection1()
+    {
+        $cmp = Numbers::naturalOrdering();
+        Range::open(3, 5, $cmp)->intersection(Range::open(5, 10, $cmp));
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function shouldThrowExceptionIfNoIntersection2()
+    {
+        $cmp = Numbers::naturalOrdering();
+        Range::closed(1, 5, $cmp)->intersection(Range::closed(6, 10, $cmp));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSpan()
+    {
+        $cmp = Numbers::naturalOrdering();
+        $res1 = Range::closedOpen(3, 10, $cmp);
+        self::assertTrue(Range::closed(3, 5, $cmp)->span(Range::open(5, 10, $cmp))->equals($res1));
+
+        $res2 = Range::closed(0, 9, $cmp);
+        self::assertTrue(Range::closed(0, 9, $cmp)->span(Range::closed(3, 4, $cmp))->equals($res2));
+
+        $res3 = Range::closed(0, 9, $cmp);
+        self::assertTrue(Range::closed(0, 5, $cmp)->span(Range::closed(3, 9, $cmp))->equals($res3));
+
+        $res4 = Range::open(3, 10, $cmp);
+        self::assertTrue(Range::open(3, 5, $cmp)->span(Range::open(5, 10, $cmp))->equals($res4));
+
+        $res5 = Range::closed(1, 10, $cmp);
+        self::assertTrue(Range::closed(1, 5, $cmp)->span(Range::closed(6, 10, $cmp))->equals($res5));
+    }
+}
